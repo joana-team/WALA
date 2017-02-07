5c5b30ab94a143324957c683d4020ec6670df970
Martin Hecker <martin.hecker@kit.edu>
2017-02-07 12:58:49 +0100

    Do not set CREATE Instantiation-Behavior for java.lang.object and all its subclasses.
    Specifically, this gives lookups a chance to fall back to the default instantiation behaviour, which is REUSE.

--- com.ibm.wala.dalvik/src/com/ibm/wala/dalvik/ipa/callgraph/androidModel/parameters/DefaultInstantiationBehavior.java
+++ com.ibm.wala.dalvik/src/com/ibm/wala/dalvik/ipa/callgraph/androidModel/parameters/DefaultInstantiationBehavior.java
@@ -134,10 +134,6 @@ public class DefaultInstantiationBehavior extends IInstantiationBehavior {
                     InstanceBehavior.CREATE,
                     Exactness.EXACT,
                     null));
-        behaviours.put(BehaviorKey.mk(TypeName.string2TypeName("Ljava/lang/Object")), new BehviourValue( // TypeReference.JavaLangObjectName is private
-                    InstanceBehavior.CREATE,
-                    Exactness.EXACT,
-                    null));
         behaviours.put(BehaviorKey.mk(
                         TypeReference.findOrCreateArrayOf(TypeReference.JavaLangObject).getName()              
                     ), new BehviourValue(
